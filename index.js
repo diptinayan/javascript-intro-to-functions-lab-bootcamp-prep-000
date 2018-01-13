a4a9576e20b2717d96b3f353029fbfeea4468f4f
Dipti Nayan <diptinayan21@gmail.com>
2018-01-13 08:36:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function sayHiToGrandma("I love you, Grandma") {
-  return string("I love you,too")
+function sayHiToGrandma(I love you, Grandma) {
+  return string(I love you,too)
 }
